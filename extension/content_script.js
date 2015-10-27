051f9ed2af0786d7740b061ca68dd1ec10396520
Thalida Noel <thalida.c.noel@gmail.com>
2015-10-27 01:47:49 -0400

    Fixed regex logic | Added enable storage functionality
    - Moved string replacement logic to new-able object
    - Implemented chrome storage
    - Added watchers for the enabled state

--- extension/content_script.js
+++ extension/content_script.js
@@ -1,51 +1,111 @@
+// Inspiration/Helpers
 // http://stackoverflow.com/questions/5904914/javascript-regex-to-replace-text-not-in-html-attributes/5904945#5904945
 // http://stackoverflow.com/questions/15604140/replace-multiple-strings-with-multiple-other-strings
 
-walk( document.body );
+var StringReplacer = function( map ){
+	console.log( 'word map:', map );
+	this.wordMap = {
+		// 'and': '--and--',
+		// 'of': '((of))',
+		'the': '((the))'
+	};
 
-function walk( node ){
-	var child, next;
+    this.regex = this.createRegex();
+};
 
-	switch ( node.nodeType ){
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
+StringReplacer.prototype = {
+	run: function( startNode ){
+		this.walk( startNode );
+	},
 
-			while( child ){
-				next = child.nextSibling;
-				walk( child );
-				child = next;
+	createRegex: function(){
+		var keysQuery = Object.keys(this.wordMap).join('|');
+    	var regexStr = '\\b(' + keysQuery + ')\\b';
+    	return new RegExp(regexStr, 'gi');
+	},
+
+	walk: function( node ){
+		var child, next;
+
+		switch ( node.nodeType ){
+			case 1:  // Element
+			case 9:  // Document
+			case 11: // Document fragment
+				child = node.firstChild;
+
+				while( child ){
+					next = child.nextSibling;
+					this.walk( child );
+					child = next;
+				}
+
+				break;
+
+			case 3: // Text node
+				this.handleTextNode( node );
+				break;
+		}
+	},
+
+	handleTextNode: function( textNode ){
+		textNode.nodeValue = this.replaceWords(textNode.nodeValue);
+	},
+
+	replaceWords: function( str ){
+		var wordMap = this.wordMap;
+
+		return str.replace(this.regex, function( matchedStr, foundStr ){
+			var replacementStr = wordMap[foundStr.toLowerCase()];
+			var result = matchedStr.replace(foundStr, replacementStr);
+
+			if( matchedStr.charAt(0) === matchedStr.charAt(0).toUpperCase() ){
+				result = result.charAt(0).toUpperCase() + result.slice(1);
 			}
 
-			break;
-
-		case 3: // Text node
-			handleText( node );
-			break;
+			return result;
+		});
 	}
-}
+};
 
-function replaceAll( str, mapObj ){
-    var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
+var init = function(){
+	var namespace = 'wordbird__';
+	var wordMapkey = namespace + 'wordMap';
 
-    return str.replace(re, function(matched){
-        return mapObj[matched.toLowerCase()];
-    });
-}
+	function getEnabledState( cb ){
+		var key = namespace + 'enabled';
+		chrome.storage.sync.get(key, function( data ){
+			return cb( data[key] );
+		});
+	}
 
-function handleText( textNode ){
-	var wordMap = {
-		'and': 'fooooo',
-		'of': 'barrrr',
-		'the': 'helloooo'
-	};
-	
-	textNode.nodeValue = replaceAll(textNode.nodeValue, wordMap);
-}
+	function getWordMap( cb ){
+		var key = namespace + 'wordMap';
+		chrome.storage.sync.get(key, function( data ){
+			return cb( data[key] );
+		});
+	}
 
-function onStorageChange( evt ){
-	console.log( evt )
-}
+	getEnabledState(function( state ){
+		var isEnabled = state;
+		if( isEnabled ){
+			getWordMap(function(map){
+				new StringReplacer( map ).run( document.body );
+			});
+		}
+	});
+};
 
-window.addEventListener('storage', onStorageChange, false);
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+	for (var key in changes) {
+    	var storageData = changes[key];
+		if( key === 'wordbird__enabled' ){
+			if( storageData.newValue === true ){
+				init();
+			} else {
+				window.location.reload( false );
+			}
+		}
+    }
+});
+
+init();
--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,22 +1,74 @@
-$(function(){
-	var storage = {
-		namespace: 'wordbird__',
-		get: function( key ){
-			return localStorage.getItem( storage.namespace + key );
-		},
-		set: function( key, val ){
-			localStorage.setItem( storage.namespace + key, val );
-			return storage.get( key );
-		}
-	};
+var Storage = function( namespace ){
+	this.namespace = namespace;
+	this.items = {};
+};
 
-	var settings = {
-		enabled: storage.get('enabled') || true
-	};
+Storage.prototype = {
+	add: function( opts, cb ){
+		this.items[opts.key] = new StorageItem( this.namespace + opts.key, opts.defaultTo );
+		return this.items[opts.key].get( cb );
+	},
+
+	get: function( key, cb ){
+		return this.items[key].get( cb );
+	},
+
+	set: function( key, value, cb ){
+		return this.items[key].set( value );
+	}
+};
+
+var StorageItem = function( key, defaultVal ){
+	var self = this;
+
+	this.key = key;
+	this.get(function( data ){
+		if( typeof data === 'undefined' ){
+			self.set( defaultVal );
+		}
+	});
+};
+
+StorageItem.prototype = {
+	get: function( cb ){
+		var key = this.key;
+		chrome.storage.sync.get(key, function( data ){
+			console.log( 'get:', data );
+			return cb( data[key] );
+		});
+	},
+
+	set: function( value, cb ){
+		var key = this.key;
+		var storage = {};
+		storage[key] = value;
+
+		chrome.storage.sync.set(storage);
+	}
+};
+
+$(function(){
+	var storage = new Storage('wordbird__');
+	storage.add({key: 'enabled', defaultTo: true}, function( state ){
+		$('.js-enabled-state').html( " " + state );
+	});
+	storage.add({key: 'wordMap', defaultTo: {}});
 
 	var $enableToggle = $('#toggleEnable');
 	$enableToggle.on('click', function(){
-		storage.set( 'enabled', !settings.enabled );
-		settings.enabled = !settings.enabled;
+		storage.get('enabled', function( currState ){
+			storage.set('enabled', !currState);
+		});
 	});
 });
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    for (var key in changes) {
+		var storageData = changes[key];
+		if( key === 'wordbird__enabled' ){
+			$('.js-enabled-state').html( " " + storageData.newValue );
+		}
+	}
+});
+
+

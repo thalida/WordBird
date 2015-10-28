a656df46f3f15514ff042f720a5a2570e9082ec6
Thalida Noel <thalida.c.noel@gmail.com>
2015-10-28 01:17:27 -0400

    Changed to use background.js instead of popup

---
+++ extension/background.js
@@ -0,0 +1,53 @@
+var toggle = {
+	key: 'isEnabled',
+	defaultState: true,
+	get: function( cb ){
+		chrome.storage.sync.get(toggle.key, function( data ){
+			var state = data[toggle.key];
+			if( typeof state === 'undefined' ){
+				state = toggle.defaultState;
+			}
+			cb( state );
+		});
+	},
+	set: function( value, cb ){
+		var key = toggle.key;
+		var storage = {};
+		storage[key] = value;
+
+		chrome.storage.sync.set(storage, function(){
+			if( chrome.runtime.lastError ){
+    			throw Error(chrome.runtime.lastError);
+    		} else {
+    			cb();
+    		}
+		});
+	}
+};
+
+var icon = {
+	paths: {
+		on: 'icons/icon-on.19.png',
+		off: 'icons/icon-off.19.png'
+	},
+	set: function( state ){
+		var type = ( state ) ? 'on' : 'off';
+    	chrome.browserAction.setIcon({ path: icon.paths[type] });
+	}
+};
+
+var events = {
+	init: function(){
+		toggle.get( icon.set );
+		chrome.browserAction.onClicked.addListener( events.onClick );
+	},
+	onClick: function ( tab ) {
+		toggle.get(function( state ){
+			toggle.set(!state, function(){
+				icon.set( !state );
+			});
+		});
+	}
+};
+
+events.init();
